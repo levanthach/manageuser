fb0c9bcd2027bd366f0a26a0374875dce324cd7a
lethach <lethach150699@gmail.com>
2020-03-22 14:57:24 +0700

    Updates

---
+++ precache-manifest.8d284b885232d24a0b185d2c4a664d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037b1986a2cd57e4ffe9709e48a02c1d",
+    "url": "/manageuser/index.html"
+  },
+  {
+    "revision": "b9e6deceba43afa251ea",
+    "url": "/manageuser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee3fc60c5e18a573cafc",
+    "url": "/manageuser/static/js/2.944c8167.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/manageuser/static/js/2.944c8167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e6deceba43afa251ea",
+    "url": "/manageuser/static/js/main.88cd1c63.chunk.js"
+  },
+  {
+    "revision": "af6578177ca7c46871ad",
+    "url": "/manageuser/static/js/runtime-main.a22e7934.js"
+  }
+]);
